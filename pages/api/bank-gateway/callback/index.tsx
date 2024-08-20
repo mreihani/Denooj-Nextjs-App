95770af966601004de8dccae75d5a078749a6270
Amin <reihani.eng@gmail.com>
2024-08-20 10:47:47 +0330

    Check bank token

--- pages/api/bank-gateway/callback/index.tsx
+++ pages/api/bank-gateway/callback/index.tsx
@@ -9,35 +9,56 @@ const corsOptions = {
 };
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-
-    async function fetchCsrfToken() {
-
-        const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
-        const url = "admin/api/payment/callback";
-
-        let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            mode: 'cors'
-        });
-        let parsedResponse = await fetchPostResponse.json();
-
-        // return back token
-        return parsedResponse.csrfToken;
-    }
-
+    
     cors(corsOptions)(req, res, async() => {
+
+        // async function fetchCsrfToken() {
+
+        //     const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
+        //     const url = "admin/api/payment/callback";
+    
+        //     let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
+        //         method: 'GET',
+        //         headers: {
+        //             'Accept': 'application/json',
+        //             'Content-Type': 'application/json',
+        //         },
+        //         credentials: 'include',
+        //         mode: 'cors'
+        //     });
+        //     let parsedResponse = await fetchPostResponse.json();
+    
+        //     // return back token
+        //     return parsedResponse.csrfToken;
+        // }
 
         async function sendCbQuery() {
 
-            let token = await fetchCsrfToken();
+            //let token = await fetchCsrfToken();
             const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
             // const url = `admin/api/payment/callback?Token=${values.Token}&OrderId=${values.OrderId}&TerminalNo=${values.TerminalNo}&RRN=${values.RRN}&status=${values.status}&HashCardNumber=${values.HashCardNumber}&Amount=${values.Amount}&SwAmount=${values.SwAmount}&STraceNo=${values.STraceNo}&DiscountedProduct=${values.DiscountedProduct}`;
             const url = 'admin/api/payment/callback';
+
+
+
+
+
+
+
+
+
+            let fetcCSRFResponse = await fetch(`${domainUrl}${url}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                mode: 'cors'
+            });
+            let parsedCsrf = await fetcCSRFResponse.json();
+            let token = parsedCsrf.csrfToken;
+
 
             let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
                 method: 'POST',
