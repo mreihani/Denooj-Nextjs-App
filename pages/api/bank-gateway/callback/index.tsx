457d18407934bf11af9eeb0cdafb40bd40c0b450
Amin <reihani.eng@gmail.com>
2024-08-20 10:10:11 +0330

    Check bank token

--- pages/api/bank-gateway/callback/index.tsx
+++ pages/api/bank-gateway/callback/index.tsx
@@ -15,7 +15,18 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 
             const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
             const values = req.body;
-            const url = `admin/api/payment/callback?param1=${JSON.stringify(values)}`;
+            const url = `admin/api/payment/callback?
+            Token=${values.Token}
+            &OrderId=${values.OrderId}
+            &TerminalNo=${values.TerminalNo}
+            &RRN=${values.RRN}
+            &status=${values.status}
+            &HashCardNumber=${values.HashCardNumber}
+            &Amount=${values.Amount}
+            &SwAmount=${values.SwAmount}
+            &STraceNo=${values.STraceNo}
+            &DiscountedProduct=${values.DiscountedProduct}
+            `;
 
             let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
                 method: 'GET',
