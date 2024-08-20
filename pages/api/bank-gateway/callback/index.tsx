cf0ccc3bb87bdc098286e31390b852f6d3795851
Amin <reihani.eng@gmail.com>
2024-08-20 13:04:27 +0330

    Check bank token

--- pages/api/bank-gateway/callback/index.tsx
+++ pages/api/bank-gateway/callback/index.tsx
@@ -13,34 +13,40 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const router = useRouter();
 
-    async function sendCbQuery() {
-        const values = req.body;
-        const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
-        const url = `admin/api/payment/callback?Token=${values.Token}&OrderId=${values.OrderId}&status=${values.status}&STraceNo=${values.STraceNo}&Amount=${values.Amount}`;
-       
-        let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            mode: 'cors'
-        });
+    cors(corsOptions)(req, res, async() => {
 
-        let parsedResponse = await fetchPostResponse.json();
+        async function sendCbQuery() {
 
-        return parsedResponse;
-    }
+            const values = req.body;
+            const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
+            const url = `admin/api/payment/callback?Token=${values.Token}&OrderId=${values.OrderId}&status=${values.status}&STraceNo=${values.STraceNo}&Amount=${values.Amount}`;
+           
+            let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                credentials: 'include',
+                mode: 'cors'
+            });
 
-    let response = await sendCbQuery();
+            let parsedResponse = await fetchPostResponse.json();
 
-   
-    if(response.status === 0) {
-        router.push('/dashboard');
-        return;
-    } else {
-        router.push('/checkout');
-        return;
-    }
+            return parsedResponse;
+        }
+
+        let response = await sendCbQuery();
+
+        //return res.json(response);
+
+        if(response.status === 0) {
+            router.push('/dashboard');
+            return;
+        } else {
+            router.push('/checkout');
+            return;
+        }
+
+    });
 };
