579325c930fe7a6688e328ca7aff650fed7db020
Amin <reihani.eng@gmail.com>
2024-08-20 12:59:53 +0330

    Check bank token

--- pages/api/bank-gateway/callback/index.tsx
+++ pages/api/bank-gateway/callback/index.tsx
@@ -11,39 +11,37 @@ const corsOptions = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
-    cors(corsOptions)(req, res, async() => {
+    const router = useRouter();
 
-        async function sendCbQuery() {
+    async function sendCbQuery() {
+        const values = req.body;
+        const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
+        const url = `admin/api/payment/callback?Token=${values.Token}&OrderId=${values.OrderId}&status=${values.status}&STraceNo=${values.STraceNo}&Amount=${values.Amount}`;
+       
+        let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            mode: 'cors'
+        });
 
-            const values = req.body;
-            const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
-            const url = `admin/api/payment/callback?Token=${values.Token}&OrderId=${values.OrderId}&status=${values.status}&STraceNo=${values.STraceNo}&Amount=${values.Amount}`;
-           
-            let fetchPostResponse = await fetch(`${domainUrl}${url}`, {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                credentials: 'include',
-                mode: 'cors'
-            });
+        let parsedResponse = await fetchPostResponse.json();
 
-            let parsedResponse = await fetchPostResponse.json();
+        return parsedResponse;
+    }
 
-            return parsedResponse;
-        }
+    let response = await sendCbQuery();
 
-        let response = await sendCbQuery();
+    //return res.json(response);
 
-        return res.json(response);
-        const router = useRouter();
-
-        if(response.status === 0) {
-            router.push('/dashboard');
-        } else {
-            router.push('/checkout');
-        }
-
-    });
+    if(response.status === 0) {
+        router.push('/dashboard');
+        return;
+    } else {
+        router.push('/checkout');
+        return;
+    }
 };
