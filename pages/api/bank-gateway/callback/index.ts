7deb1158570ada3634ce85bea67f4b7a6fcaf0bc
Amin <reihani.eng@gmail.com>
2024-08-20 07:24:50 +0330

    Check bank token

---
+++ pages/api/bank-gateway/callback/index.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    
+    const sendToken = async () => {
+        const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
+        const url = "admin/api/payment/callback";
+        const values = req;
+
+        const response = await fetch(`${domainUrl}${url}`, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include',
+            mode: 'cors',
+            body: JSON.stringify(values),
+        });
+
+        const data = await response.json();
+        return data.csrfToken;
+    };
+
+    await sendToken();
+}
---
+++ pages/api/bank-gateway/confirmation/index.ts
@@ -0,0 +1,8 @@
+// import { useRouter } from 'next/router';
+
+// export default async function handler(req, res) {
+
+//     const router = useRouter();
+//   const { transactionId, status } = router.query;
+    
+// }
