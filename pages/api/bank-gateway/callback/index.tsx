a8c900332ace894bbcd93e7c074bfd48c8e33dd2
Amin <reihani.eng@gmail.com>
2024-08-20 07:58:27 +0330

    Check bank token

--- pages/api/bank-gateway/callback/index.tsx
+++ pages/api/bank-gateway/callback/index.tsx
@@ -2,25 +2,27 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     
-    const sendToken = async () => {
-        const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
-        const url = "admin/api/payment/callback";
-        const values = req;
+    // const sendToken = async () => {
+    //     const domainUrl = process.env.NEXT_PUBLIC_DOMAIN_URI;
+    //     const url = "admin/api/payment/callback";
+    //     const values = req;
 
-        const response = await fetch(`${domainUrl}${url}`, {
-            method: 'GET',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include',
-            mode: 'cors',
-            body: JSON.stringify(values),
-        });
+    //     const response = await fetch(`${domainUrl}${url}`, {
+    //         method: 'GET',
+    //         headers: {
+    //             'Accept': 'application/json',
+    //             'Content-Type': 'application/json',
+    //         },
+    //         credentials: 'include',
+    //         mode: 'cors',
+    //         body: JSON.stringify(values),
+    //     });
 
-        const data = await response.json();
-        return data.csrfToken;
-    };
+    //     const data = await response.json();
+    //     return data.csrfToken;
+    // };
 
-    await sendToken();
+    // await sendToken();
+
+    return res.json('hello');
 }
