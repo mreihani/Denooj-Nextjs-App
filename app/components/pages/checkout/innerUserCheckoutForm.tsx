e5be1e31d291c8a825d10eae80954399b1c25088
Amin <reihani.eng@gmail.com>
2024-08-21 12:30:35 +0330

    Check bank token

--- app/components/pages/checkout/innerUserCheckoutForm.tsx
+++ app/components/pages/checkout/innerUserCheckoutForm.tsx
@@ -66,22 +66,36 @@ const InnerUserCheckoutForm = (props: FormikProps<UserCheckoutFormInterface>) =>
                     </button>
                 } */}
 
-                <>
-                    <button type="submit" onClick={() => changeBtnStatus()} className={`${loading ? 'text-decoration-none d-flex align-items-center btn btn-dark rounded-pill mt-3 border-0' : 'btn_primary mt-3 mb-5 border-0'}`} disabled>
-                        {
-                            loading ? 'صبر کنید'
-                            :
-                            `
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-credit-card" viewBox="0 0 16 16">
-                                    <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
-                                    <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-                                </svg>
-                                &nbsp;پرداخت
-                            `
-                        }
-                        <span className="spinner-grow spinner-grow-sm me-2" role="status" aria-hidden="true"></span>
-                    </button>
-                </>
+                {
+                    <>
+                        <button 
+                            type="submit" 
+                            onClick={() => changeBtnStatus()} 
+                            className={`${loading ? 'text-decoration-none d-flex align-items-center btn btn-dark rounded-pill mt-3 border-0' : 'btn_primary mt-3 mb-5 border-0'}`} 
+                            disabled={loading} // Add disabled attribute based on the loading state
+                        >
+                            {
+                                loading ? 
+                                (
+                                    <>
+                                        صبر کنید
+                                        <span className="spinner-grow spinner-grow-sm me-2" role="status" aria-hidden="true"></span>
+                                    </>
+                                )
+                                :
+                                (
+                                    <>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-credit-card" viewBox="0 0 16 16">
+                                            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v1h14V4a1 1 0 0 0-1-1zm13 4H1v5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1z"/>
+                                            <path d="M2 10a1 1 0 0 1 1-1h1a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                                        </svg>
+                                        &nbsp;پرداخت
+                                    </>
+                                )
+                            }
+                        </button>
+                    </>
+                }
                    
             </>  
         </Form>
